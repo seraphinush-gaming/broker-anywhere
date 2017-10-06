93356e09d2e374e338a1c51a635d46c57c2d917b
seraphinush <30970630+seraphinush@users.noreply.github.com>
2017-10-06 12:00:41 +0900

    Update index.js
    - Personalized code aesthetics
    - Incorporated Pinkie Pie's Command module

--- index.js
+++ index.js
@@ -1,10 +1,38 @@
+// OPCODE REQUIRED :
+// - S_NPC_MENU_SELECT
+
 module.exports = function BrokerAnywhere(dispatch){
-    const chatHook = event => {
+    
+    // command
+	try {
+		const Command = require('command')
+		const command = Command(dispatch)
+		command.add('broker', () => {
+			broker()
+			send(`[broker-anywhere] : Summoned.`)
+		})
+		function send(msg) {
+			command.message(`<font color="#FFFFFF">` + msg + `</font>`)
+		}
+	} catch (e) {
+		console.log(`[ERROR] -- broker-anywhere module --`)
+    }
+
+	// via chat
+	// implemented in Command module
+	/* 
+	const chatHook = event => {
         if(!event.message.toLowerCase().includes('!broker')) return
-        
-        dispatch.toClient('S_NPC_MENU_SELECT', 1, {type:28})
+		broker()
         return false
     }
     dispatch.hook('C_CHAT', 1, chatHook)
-    dispatch.hook('C_WHISPER', 1, chatHook)
+	dispatch.hook('C_WHISPER', 1, chatHook) 
+	*/
+	 
+    // code
+    function broker() {
+        dispatch.toClient('S_NPC_MENU_SELECT', 1, { type: 28 })
+	}
+
 }
