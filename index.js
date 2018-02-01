da8716fe4a202569632f595256355f7f6e021af6
seraph <30970630+seraphinush-gaming@users.noreply.github.com>
2018-02-01 23:58:57 +0900

    1.35 r:00

--- index.js
+++ index.js
@@ -1,31 +1,15 @@
 // OPCODE REQUIRED :
 // - S_NPC_MENU_SELECT
 
-// Version 1.34 r:01
+// Version 1.35 r:00
 
-module.exports = function BrokerAnywhere(d){
-
-    // code
-    function broker() { d.toClient('S_NPC_MENU_SELECT', { type: 28 }) }
+module.exports = function CmdBroker(d){
 
     // command
 	try {
 		const Command = require('command')
 		const command = Command(d)
-		// KR
-		command.add('거래', () => {
-			broker()
-			send(`소환됬습니다.`.clr('56B4E9'))
-		})
-		// NA
-		command.add('broker', () => {
-			broker()
-			send(`Summoned.`.clr('56B4E9'))
-		})
-		function send(msg) { command.message(`[command-broker] : ` + [...arguments].join('\n\t - ')) }
-	} catch (e) { console.log(`[ERROR] -- command-broker module --`) }
+		command.add(['broker', '거래'], () => { d.toClient('S_NPC_MENU_SELECT', { type: 28 }) })
+	} catch (e) { console.log(`[ERROR] -- cmd-broker module --`) }
 
 }
-
-// credit : https://github.com/Some-AV-Popo
-String.prototype.clr = function (hexColor) { return `<font color="#${hexColor}">${this}</font>` }
