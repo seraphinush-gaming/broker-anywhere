329e77bff37d35f5979056cdb7d75dc41177847a
seraph <30970630+seraphinush-gaming@users.noreply.github.com>
2018-03-20 23:20:08 +0900

    1.36 r:00

--- index.js
+++ index.js
@@ -1,15 +1,11 @@
-// OPCODE REQUIRED :
-// - S_NPC_MENU_SELECT
+// Version 1.36 r:00
 
-// Version 1.35 r:01
+const Command = require('command')
 
 module.exports = function CmdBroker(d){
+	const command = Command(d)
 
     // command
-	try {
-		const Command = require('command')
-		const command = Command(d)
-		command.add(['broker', '거래'], () => { d.toClient('S_NPC_MENU_SELECT', { type: 28 }) })
-	} catch (e) { console.log(`[ERROR] -- cmd-broker module --`) }
+	command.add(['broker', '거래'], () => { d.toClient('S_NPC_MENU_SELECT', { type: 28 }) })
 
 }
